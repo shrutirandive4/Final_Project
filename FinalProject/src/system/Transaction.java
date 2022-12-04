c34758d1c8f34a94f861c5d5b0d11ba92fcd9c20
Davleen <73703041+Davleen@users.noreply.github.com>
2022-12-04 00:47:42 -0500

    Added java classes

---
+++ FinalProject/src/system/Transaction.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package system;
+
+/**
+ *
+ * @author Davleen kaur
+ */
+public class Transaction {
+    private int bookingId;
+    private int transactionId;
+    private int transactionAmount;
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public int getTransactionAmount() {
+        return transactionAmount;
+    }
+
+    public void setTransactionAmount(int transactionAmount) {
+        this.transactionAmount = transactionAmount;
+    }
+    
+}
